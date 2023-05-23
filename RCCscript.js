269d6016057ee7136ece06b5294f5746bc42aaac
Omarichi <129509411+Omarichi@users.noreply.github.com>
2023-05-23 11:16:48 +0400

    Add files via upload

---
+++ RCCscript.js
@@ -0,0 +1,64 @@
+// The SlideShow javascript
+
+let slideIndex = 0;
+showSlides();
+
+
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+// navbar javascript
+
+let searchBtn = document.querySelector('.searchBtn');
+let closeBtn = document.querySelector('.closeBtn');
+let searchBox = document.querySelector('.searchBox');
+let navigation = document.querySelector('.navigation');
+let menuToggle = document.querySelector('.menuToggle');
+let header = document.querySelector('header');
+
+searchBtn.onclick = function () {
+    searchBox.classList.add('active');
+    closeBtn.classList.add('active');
+    searchBtn.classList.add('active');
+    menuToggle.classList.add('hide');
+    header.classList.remove('open');
+
+}
+
+closeBtn.onclick = function () {
+    searchBox.classList.remove('active');
+    closeBtn.classList.remove('active');
+    searchBtn.classList.remove('active');
+    menuToggle.classList.remove('hide');
+}
+
+menuToggle.onclick = function () {
+    header.classList.toggle('open');
+    searchBox.classList.remove('active');
+    closeBtn.classList.remove('active');
+    searchBtn.classList.remove('active');
+}
+
+//popup javascript
+
+function toggle() {
+
+    let toggle = document.querySelector('.popup-container')
+
+    toggle.classList.toggle('toggle');
+}
